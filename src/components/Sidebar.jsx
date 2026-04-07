fbeae21136b1f898d42bbd62a08d181b671b5881
MatiasAbregu <matiasabregu506@gmail.com>
2026-04-07 09:01:03 -0300

    Initial sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,24 +1,39 @@
 import React from 'react'
 import '../styles/components/Sidebar.css'
+import { useLocation } from 'react-router'
 
 export const Sidebar = () => {
-  return (
-    /*
-    - Plan de estudio
-    - Materias
-    - Correlatividades
-    - Asistencias
-    - Notas
-    - Mesa de examenes
-    - Inscripciones
-    */
-    <div className='sidebar'>
-        <ul>
-            <a href=""><li>Estudiantes</li></a>
-            <a href=""><li>Docentes</li></a>
-            <a href=""><li>Usuarios</li></a>
-            <a href=""><li>Carrreras</li></a>
-        </ul>
-    </div>
-  )
+
+    const url = useLocation();
+
+    return (
+        /*
+        - Plan de estudio
+        - Materias
+        - Correlatividades
+        - Asistencias
+        - Notas
+        - Mesa de examenes
+        - Inscripciones
+        */
+        <div className='sidebar'>
+            <ul>
+                <a href="/inicio" className={url.pathname == "/inicio" ? "activated" : ""}>
+                    <span class="material-symbols-outlined">home</span><li>Inicio</li>
+                </a>
+                <a href="" className={``}>
+                    <span class="material-symbols-outlined">school</span><li>Estudiantes</li>
+                </a>
+                <a href="" className={``}>
+                    <span class="material-symbols-outlined">cast_for_education</span><li>Docentes</li>
+                </a>
+                <a href="" className={``}>
+                    <span class="material-symbols-outlined">supervised_user_circle</span><li>Usuarios</li>
+                </a>
+                <a href="" className={``}>
+                    <span class="material-symbols-outlined">history_edu</span><li>Carrreras</li>
+                </a>
+            </ul>
+        </div>
+    )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Footer } from '../components/Footer';
 import { Sidebar } from '../components/Sidebar';
 
 export const Home = () => {
-  const [user, setUser] = useState({username: "Marcos", role: "Administrador"});
+    const [user, setUser] = useState({ username: "Marcos", role: "Administrador" });
 
     useEffect(() => {
         document.title = "ISPT - Menú"
@@ -13,11 +13,13 @@ export const Home = () => {
     return (
         <article className="homePage">
             <Sidebar />
-            <h2>¡Bienvenido {user.username}!</h2>
-            <div className="buttonContainer">
-                
+            <div className='homePageContainer'>
+                <h2>¡Bienvenido {user.username}!</h2>
+                <div className="buttonContainer">
+
+                </div>
+                <Footer />
             </div>
-            <Footer />
         </article>
     );
 }
