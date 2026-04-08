5702b28feed6280ea8b863b2a4f2ab19dbed584d
MatiasAbregu <matiasabregu506@gmail.com>
2026-04-08 01:14:57 -0300

    Students

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,18 +18,18 @@ export const Home = () => {
             <div className='homePageContainer'>
                 <h2>¡Bienvenido {user.username}!</h2>
                 <div className='mainContent'>
-                <div className="chartsContainer">
-                    {user.role === "Estudiante" && (
-                        <>
-                            <AverageGradesChart materias={["Programación", "Matemática", "Física"]} notas={[8, 9, 7]} />
-                            <SubjectsChart aprobadas={2} pendientes={1} />
-                        </>
-                    )}
-                </div>
-                <div className="enrollmentContainer">
-                    <h3>Inscripciones</h3>
-                    <p>Próximamente</p>
-                </div>
+                    <div className="chartsContainer">
+                        {user.role === "Estudiante" && (
+                            <>
+                                <AverageGradesChart materias={["Programación", "Matemática", "Física"]} notas={[8, 9, 7]} />
+                                <SubjectsChart aprobadas={2} pendientes={1} />
+                            </>
+                        )}
+                    </div>
+                    <div className="enrollmentContainer">
+                        <h3>Inscripciones</h3>
+                        <p>Próximamente</p>
+                    </div>
                 </div>
                 <Footer />
             </div>
