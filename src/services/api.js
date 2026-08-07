91a17b18adc86fad4f7f9360c5e6c8de8824d04d
MatiasAbregu <matiasabregu506@gmail.com>
2026-08-07 08:31:47 -0300

    env

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const API_URL = import.meta.env.VITE_API_URL_DEPLOY;
 
 const api = axios.create({
-    baseURL: "http://localhost:5293/api-v1",
+    baseURL: {API_URL},
 });
 
 let refreshTokenFunction = null;
